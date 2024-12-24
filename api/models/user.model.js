0750bf938a056b0b5c48fc83166a827cc0e8b635
U-VISHAL <vishalvishaal55@gmail.com>
2024-12-24 15:16:29 +0530

    created api route for signup

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    oassword:{
+    password:{
         type:String,
         required: true,
     },
